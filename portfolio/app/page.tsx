892f83a05e1978c38c444c388fb1da74977fce70
B-Kinda <baptiste.lebreton97@gmail.com>
2025-12-03 16:32:29 +0100

    /clear

--- portfolio/app/page.tsx
+++
@@ -1,8 +0,0 @@
-// page.tsx est la page d'accueil
-"use client";
-
-import Hero from "./components/Hero";
-
-export default function Home() {
-	return <Hero />;
-}
--- portfolio/app/types/ButtonProps.ts
+++
@@ -1,9 +0,0 @@
-// -------------------- Types externes bouton --------------------
-export interface ButtonProps {
-	text?: string;
-	activeText?: string;
-	isActive?: boolean;
-	onToggle?: (newState: boolean) => void;
-	href?: string;
-	className?: string;
-}
